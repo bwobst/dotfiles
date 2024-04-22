37d85977c036de9ccfdcce6694de6ff8cef80b5b
Brian Wobst <bwobst@gmail.com>
2024-04-21 23:24:33 -0700

    Update fish config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -49,17 +49,59 @@ fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin";
 ####################################################################################################
 ## Aliases
 ####################################################################################################
-alias catpack="cat package.json"
-alias gop="git open"
-alias gpom="git pull origin main"
-alias gs="git status"
-alias ns="npm run start"
-alias nt="npm run test"
-alias ntw="npm run test:watch"
-alias ntc="npm run test:coverage"
-alias gcaw="git add -A && git commit -m \"WIP\""
-alias gmetro="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(normal)%aD%C(reset) %C(green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(bold white)%s%C(reset) %C(dim white)- %an%C(reset)' --all $argv"
-alias k="kubectl"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+
+alias dl "cd ~/Downloads"
+alias dt "cd ~/Desktop"
+alias c "cd ~/code"
+
+alias g "git"
+alias gop "git open"
+alias gpom "git pull origin main"
+alias gs "git status"
+alias gcaw "git add -A && git commit -m \"WIP\""
+alias gmetro "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(normal)%aD%C(reset) %C(green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(bold white)%s%C(reset) %C(dim white)- %an%C(reset)' --all $argv"
+
+alias catpack "cat package.json | jq"
+
+alias ns "npm run start"
+alias nt "npm run test"
+alias ntw "npm run test:watch"
+alias ntc "npm run test:coverage"
+
+alias k "kubectl"
+
+# List all files colorized in long format
+alias l="ls -lFG"
+
+# List only directories
+alias lsd="ls -lFG | grep --color=never '^d'"
+
+# Always enable colored `grep` output
+# Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
+alias grep='grep --color=auto'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
 ####################################################################################################
 ## Apex Platform Environment Variables
---
+++ private_dot_config/fish/functions/custom.fish
@@ -0,0 +1,11 @@
+# Reload the shell (i.e. invoke as a login shell)
+function reload
+    exec $SHELL -l
+end
+
+# Print each PATH entry on a separate line
+function path
+    for p in (string split ":" $PATH)
+        echo $p
+    end
+end
