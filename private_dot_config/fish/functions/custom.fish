a880e410ec49047982ab16588380773c3796552b
Brian Wobst <bwobst@gmail.com>
2024-04-24 10:43:22 -0700

    Remove path function

--- private_dot_config/fish/functions/custom.fish
+++ private_dot_config/fish/functions/custom.fish
@@ -3,13 +3,6 @@ function reload
     exec $SHELL -l
 end
 
-# Print each PATH entry on a separate line
-function path
-    for p in (string split ":" $PATH)
-        echo $p
-    end
-end
-
 function pathls
     string replace : '\n' $PATH
 end
