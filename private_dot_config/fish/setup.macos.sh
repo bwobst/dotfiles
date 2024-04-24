45a6ce0b83120a5cd2a28866e8a173f858d2ca64
Brian Wobst <bwobst@gmail.com>
2024-04-24 11:28:26 -0700

    Add setup.sh for fish and dotfiles

--- private_dot_config/fish/setup.macos.sh
+++ private_dot_config/fish/setup.macos.sh
@@ -1,10 +1,8 @@
 # Install fish shell
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install fish
+brew install fish
 
 # Set fish as default shell
-sudo chsh -s /usr/bin/fish
+sudo chsh -s /opt/homebrew/bin/fish
 
 # Install zoxide
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
---
+++ private_dot_config/fish/setup.sh
@@ -0,0 +1,21 @@
+if [[ "$(uname)" == "Linux" ]]; then
+    echo "Operating system is Linux."
+elif [[ "$(uname)" == "Darwin" ]]; then
+    echo "Operating system is macOS."
+else
+    echo "Operating system is something else."
+fi
+
+# Install and initialize chezmoi and dotfiles
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply bwobst
+
+# Set fish as default shell
+sudo chsh -s $(which fish)
+
+# Install zoxide
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
+
