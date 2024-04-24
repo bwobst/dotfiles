7dea0d335e04083ad5ad598b8dd8198abca3b80a
Brian Wobst <bwobst@gmail.com>
2024-04-24 10:59:16 -0700

    Add pathls function

--- private_dot_config/fish/functions/custom.fish
+++ private_dot_config/fish/functions/custom.fish
@@ -4,12 +4,6 @@ function reload
 end
 
 # Print each PATH entry on a separate line
-function path
-    for p in (string split ":" $PATH)
-        echo $p
-    end
-end
-
 function pathls
     string replace : '\n' $PATH
 end
