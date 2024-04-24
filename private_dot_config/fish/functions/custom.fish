696ffb2b3da7df2dda5ecf1bb15f1a548a67aca2
Brian Wobst <bwobst@gmail.com>
2024-04-24 11:04:41 -0700

    Remove unused `reload` fish function. Add git-open script.

--- private_dot_config/fish/functions/custom.fish
+++ private_dot_config/fish/functions/custom.fish
@@ -1,8 +1,3 @@
-# Reload the shell (i.e. invoke as a login shell)
-function reload
-    exec $SHELL -l
-end
-
 # Print each PATH entry on a separate line
 function pathls
     string replace : '\n' $PATH
