61699b318e7a0a65725378d6b7f06dec8a00fa5d
Brian Wobst <bwobst@gmail.com>
2025-02-05 11:10:29 -0800

    Update .config/fish/conf.d/fnm.fish

--- private_dot_config/fish/conf.d/fnm.fish
+++ private_dot_config/fish/conf.d/fnm.fish
@@ -1,7 +1,1 @@
-
-# fnm
-set FNM_PATH "/Users/brianwobst/Library/Application Support/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-end
+fnm env --use-on-cd --shell fish | source
