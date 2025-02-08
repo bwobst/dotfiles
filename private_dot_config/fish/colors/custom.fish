e6498dffc0f4055c90039493cf61781659532474
Brian Wobst <bwobst@gmail.com>
2025-02-07 17:05:09 -0800

    Add .config/fish/colors/custom.fish

---
+++ private_dot_config/fish/colors/custom.fish
@@ -0,0 +1,38 @@
+set -Ux fish_color_autosuggestion 747369
+set -Ux fish_color_cancel --reverse
+set -Ux fish_color_command 87ff5f
+set -Ux fish_color_comment ff0000
+set -Ux fish_color_cwd green
+set -Ux fish_color_cwd_root red
+set -Ux fish_color_end cc99cc
+set -Ux fish_color_error f2777a
+set -Ux fish_color_escape 66cccc
+set -Ux fish_color_history_current --bold
+set -Ux fish_color_host normal
+set -Ux fish_color_host_remote
+set -Ux fish_color_keyword
+set -Ux fish_color_match 6699cc
+set -Ux fish_color_normal normal
+set -Ux fish_color_operator 6699cc
+set -Ux fish_color_option
+set -Ux fish_color_param d3d0c8
+set -Ux fish_color_quote ffcc66
+set -Ux fish_color_redirection d3d0c8
+set -Ux fish_color_search_match 'bryellow'  '--background=brblack'
+set -Ux fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -Ux fish_color_status red
+set -Ux fish_color_user brgreen
+set -Ux fish_color_valid_path --underline
+set -Ux fish_pager_color_background
+set -Ux fish_pager_color_completion normal
+set -Ux fish_pager_color_description B3A06D
+set -Ux fish_pager_color_prefix 'normal'  '--bold'  '--underline'
+set -Ux fish_pager_color_progress 'brwhite'  '--background=cyan'
+set -Ux fish_pager_color_secondary_background
+set -Ux fish_pager_color_secondary_completion
+set -Ux fish_pager_color_secondary_description
+set -Ux fish_pager_color_secondary_prefix
+set -Ux fish_pager_color_selected_background --background=brblack
+set -Ux fish_pager_color_selected_completion
+set -Ux fish_pager_color_selected_description
+set -Ux fish_pager_color_selected_prefix
